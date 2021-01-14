d0a2ab4bc3195546214257743a84373506ee7c41
theo <teddyhoover@gmail.com>
2021-01-14 15:14:12 -0600

    commit tree 39c644b3b9af1e0c4765f670107c5d943dacd433

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,47 @@
-class Student
+require 'pry'
 
+class Student
+  attr_reader :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+    
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name,
+      grade
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?);
+      SQL
+    DB[:conn].execute(sql, name, grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(atr = {})
+    #binding.pry
+    student = new(atr[:name], atr[:grade])
+    student.save
+    student
+  end
   
 end
